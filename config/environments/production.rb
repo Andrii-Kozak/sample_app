a61d4249a6f1c91103440be99741d867a4ab8fcb
Andrii-Kozak <kozakdocs@gmail.com>
2020-07-27 14:36:44 +0300

    setup mailer 2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,20 +64,34 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
+  
+  # config.action_mailer.raise_delivery_errors = true
+  # config.action_mailer.delivery_method = :smtp
+  # host = 'https://protected-cliffs-96661.herokuapp.com'
+  # config.action_mailer.default_url_options = { host: host }
+  # config.action_mailer.smtp_settings = {
+  #   :address => 'smtp.gmail.com',
+  #   :port => 587,
+  #   :domain => 'gmail.com',
+  #   :user_name => 'kozak.email1111@gmail.com',
+  #   :password => '&[{}(=*)',
+  #   :authentication => :login,
+  #   :enable_starttls_auto => true
+  # }
+
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   host = 'https://protected-cliffs-96661.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
-  config.action_mailer.smtp_settings = {
-    :address => 'smtp.gmail.com',
-    :port => 587,
-    :domain => 'gmail.com',
-    :user_name => 'kozak.email1111@gmail.com',
-    :password => '&[{}(=*)',
+  ActionMailer::Base.smtp_settings = {
+    :address => 'smtp.sendgrid.net',
+    :port => '587',
+    :user_name => ENV['SENDGRID_USERNAME'],
+    :password => ENV['SENDGRID_PASSWORD'],
     :authentication => :login,
+    :domain => 'heroku.com',
     :enable_starttls_auto => true
   }
-
   # config.action_mailer.raise_delivery_errors = false
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
